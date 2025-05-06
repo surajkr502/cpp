6eea7dae6d4634e33825a97c8dc9662112912beb
surajkr502 <sawsurajkumar502@gmail.com>
2025-05-06 18:34:23 +0530

    new

--- c.cpp
+++ c.cpp
@@ -96,42 +96,42 @@ int main()
   // {
   //   cout<<"Invalid no.";
   // }
-  int path;
-cout<<"Enter a path:";
-cin>>path;
-if (path==1)
-{
-  cout<<"Enter a path:";
-cin>>path;
-if (path==4)
-{
- cout<<"Arrived"; 
-}
-else if (path==2)
-{
-  cout<<"Enter a path:";
-  cin>>path;
-}
-else
-{
-  cout<<"Enter a path:";
-  cin>>path;
-}
- if (path==4)
-{
-  cout<<"Arrived";
-}
-else if (path==3)
-{
-  cout<<"Enter a path:";
-  cin>>path;
-  if (path==4)
-  {
-    cout<<"Arrived";
-  }
+//   int path;
+// cout<<"Enter a path:";
+// cin>>path;
+// if (path==1)
+// {
+//   cout<<"Enter a path:";
+// cin>>path;
+// if (path==4)
+// {
+//  cout<<"Arrived"; 
+// }
+// else if (path==2)
+// {
+//   cout<<"Enter a path:";
+//   cin>>path;
+// }
+// else
+// {
+//   cout<<"Enter a path:";
+//   cin>>path;
+// }
+//  if (path==4)
+// {
+//   cout<<"Arrived";
+// }
+// else if (path==3)
+// {
+//   cout<<"Enter a path:";
+//   cin>>path;
+//   if (path==4)
+//   {
+//     cout<<"Arrived";
+//   }
   
 
-}
+// }
 
 
 
@@ -143,15 +143,85 @@ else if (path==3)
     
 //   }
   
-}
+// }
 
 
 // else
 // {
 //   cout<<"Invalide route"
 // }
+// string input;
+// cout<<"Enter a web address:";
+// cin>>input;
+// if (input==www.tourism.com)
+// {
+//   cout<<
+// }
+// int mathod ,A;
+// cout<<"Enter a number 1 or 2:";
+// int input;
+// cout<<"Enter meter reading:";
+// cin>>input;
+// if (input>0 && input<=50)
+// {
+//   cout<<50*10;
+// }
+// else if (input>)
+// {
+//   /* code */
+// }
+// switch (expression)
+// {
+// case 1:
+//   /* code */
+//   break;
+
+// default:
+//   break;
+// }
+
+// diadvantage of switch
+// jumping statement (go to)
+
+//  int n,a,sum=0;
+//  cout<<"Enter an Integer no.:";
+//  cin>>n;
+//  for(a=1;a<=n;a++)
+// {
+//   sum=sum+a;
+  
+// }
+// cout<<"Sum of Given Integer="<<sum;
+// using two variables
+// int i,sum;
+// cout<<"Enter an Integer:";
+// cin>>i;
+// for(sum=0;i>=1;i--)
+// {
+//   sum=sum+i;
+// }
+// cout<<sum;
+// int i,fact;
+// cout<<"Enter an integer:";
+// cin>>i;
+// for(fact=1;i>=1;i--)
+// {
+//   fact=fact*i;
+// }
+// cout<<"Factorial is:"<<fact;
+int i,sum,rem;
+cout<<"Enter Integer:";
+cin>>i;
+for(sum=0;i>0;i/10)
+{
+  rem=i%10;
+  sum=sum+rem; 
+}
+cout<<sum;
 
 
 }
+
+
 
   
