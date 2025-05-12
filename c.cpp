ef5ba69823e8affe9f89fb76faadce205e2deda7
surajkr502 <sawsurajkumar502@gmail.com>
2025-05-12 12:50:03 +0530

    new

--- c.cpp
+++ c.cpp
@@ -5,11 +5,10 @@
 //     int a=30;
 //     int b=20;
 //     cout<<a%b;
-    
-    
+
 // }
-//areas
-//circle,square,rectangle,triangle
+// areas
+// circle,square,rectangle,triangle
 // #include<iostream>
 // using namespace std;
 // int main()
@@ -32,32 +31,32 @@
 // #include<iostream>
 //   using namespace std;
 //   int main()
-  // {
-  //   int hindi,english,math,science;
-  //   cout<<"Enter Marks of hin,eng,math and sci:";
-  //   cin>>hindi>>english>>math>>science;
-  //   cout<<((hindi+english+math+science)*100)/400;
-  // }
-  // {
-    // int b;
-    // cout<<sizeof(int);
-    // float a;
-    // cout<<sizeof(float);
-    // char c;
-    // cout<<sizeof(char);
-    // cout<<&c;
-    // cout<<(int)numeric_limits <char>::max();
-    // int a=5;
-    // int b=3;
-    // int c=1;
-    // cout<<(ab);
-    // int pass;
-  
-    // cout<<"Enter a ATM password";
-    // cin>>pass;
-    // (pass==0123)?cout<<"correct pass":cout<<"incorrect";
+// {
+//   int hindi,english,math,science;
+//   cout<<"Enter Marks of hin,eng,math and sci:";
+//   cin>>hindi>>english>>math>>science;
+//   cout<<((hindi+english+math+science)*100)/400;
 // }
-#include<iostream>
+// {
+// int b;
+// cout<<sizeof(int);
+// float a;
+// cout<<sizeof(float);
+// char c;
+// cout<<sizeof(char);
+// cout<<&c;
+// cout<<(int)numeric_limits <char>::max();
+// int a=5;
+// int b=3;
+// int c=1;
+// cout<<(ab);
+// int pass;
+
+// cout<<"Enter a ATM password";
+// cin>>pass;
+// (pass==0123)?cout<<"correct pass":cout<<"incorrect";
+// }
+#include <iostream>
 using namespace std;
 int main()
 {
@@ -96,132 +95,129 @@ int main()
   // {
   //   cout<<"Invalid no.";
   // }
-//   int path;
-// cout<<"Enter a path:";
-// cin>>path;
-// if (path==1)
-// {
-//   cout<<"Enter a path:";
-// cin>>path;
-// if (path==4)
-// {
-//  cout<<"Arrived"; 
-// }
-// else if (path==2)
-// {
-//   cout<<"Enter a path:";
-//   cin>>path;
-// }
-// else
-// {
-//   cout<<"Enter a path:";
-//   cin>>path;
-// }
-//  if (path==4)
-// {
-//   cout<<"Arrived";
-// }
-// else if (path==3)
-// {
-//   cout<<"Enter a path:";
-//   cin>>path;
-//   if (path==4)
+  // int n;
+  // cout << "Enter a no.:";
+  // cin >> n;
+  // if (n >= 0)
+  // {
+  //   cout << "Number is positive.";
+  // }
+  // else if (n < 0)
+  // {
+  //   cout << "Number is Negative.";
+  // }
+
+  // string input;
+  // cout<<"Enter a web address:";
+  // cin>>input;
+  // if (input==www.tourism.com)
+  // {
+  //   cout<<
+  // }
+  // int mathod ,A;
+  // cout<<"Enter a number 1 or 2:";
+  // int input;
+  // cout<<"Enter meter reading:";
+  // cin>>input;
+  // if (input>0 && input<=50)
+  // {
+  //   cout<<50*10;
+  // }
+  // else if (input>)
+  // {
+  //   /* code */
+  // }
+  // switch (expression)
+  // {
+  // case 1:
+  //   /* code */
+  //   break;
+
+  // default:
+  //   break;
+  // }
+
+  // diadvantage of switch
+  // jumping statement (go to)
+
+  //  int n,a,sum=0;
+  //  cout<<"Enter an Integer no.:";
+  //  cin>>n;
+  //  for(a=1;a<=n;a++)
+  // {
+  //   sum=sum+a;
+
+  // }
+  // cout<<"Sum of Given Integer="<<sum;
+  // using two variables
+  // int i,sum;
+  // cout<<"Enter an Integer:";
+  // cin>>i;
+  // for(sum=0;i>=1;i--)
+  // {
+  //   sum=sum+i;
+  // }
+  // cout<<sum;
+  // int i,fact;
+  // cout<<"Enter an integer:";
+  // cin>>i;
+  // for(fact=1;i>=1;i--)
+  // {
+  //   fact=fact*i;
+  // }
+  // cout<<"Factorial is:"<<fact;
+  
+  // alt+shift+downkey
+  // int a=1,table;
+  // cout<<"Enter an no.:";
+  // cin>>table;
+  // do
+  // {
+  //  table=table*a;
+  //  cout<<table;
+  //  a++;
+
+  // } while (a<=10);
+//   int n1=10,n2=9;
+//   int i=0;
+//   for(n1=i,i<=0,++i)
 //   {
-//     cout<<"Arrived";
+// cout<<i++;
 //   }
-  
-
-// }
 
 
 
 
 
-
-//  if(path==2)
+// int i,sum,rem;
+//   cout<<"Enter three Integer no.:";
+//   cin>>i;
+//   for(sum=0;i>0;i/10)
 //   {
-    
+//     rem=i%10;
+//     sum=sum+rem;
 //   }
-  
-// }
-
-
-// else
-// {
-//   cout<<"Invalide route"
-// }
-// string input;
-// cout<<"Enter a web address:";
-// cin>>input;
-// if (input==www.tourism.com)
-// {
-//   cout<<
-// }
-// int mathod ,A;
-// cout<<"Enter a number 1 or 2:";
-// int input;
-// cout<<"Enter meter reading:";
-// cin>>input;
-// if (input>0 && input<=50)
-// {
-//   cout<<50*10;
-// }
-// else if (input>)
-// {
-//   /* code */
-// }
-// switch (expression)
-// {
-// case 1:
-//   /* code */
-//   break;
-
-// default:
-//   break;
-// }
-
-// diadvantage of switch
-// jumping statement (go to)
-
-//  int n,a,sum=0;
-//  cout<<"Enter an Integer no.:";
-//  cin>>n;
-//  for(a=1;a<=n;a++)
-// {
-//   sum=sum+a;
-  
-// }
-// cout<<"Sum of Given Integer="<<sum;
-// using two variables
-// int i,sum;
-// cout<<"Enter an Integer:";
-// cin>>i;
-// for(sum=0;i>=1;i--)
-// {
-//   sum=sum+i;
-// }
-// cout<<sum;
-// int i,fact;
-// cout<<"Enter an integer:";
-// cin>>i;
-// for(fact=1;i>=1;i--)
-// {
-//   fact=fact*i;
-// }
-// cout<<"Factorial is:"<<fact;
-int i,sum,rem;
-cout<<"Enter Integer:";
-cin>>i;
-for(sum=0;i>0;i/10)
+//   cout<<sum;
+ int number=121; 
+int org=number;
+int digit,rev=0;
+while(number!=0)
 {
-  rem=i%10;
-  sum=sum+rem; 
+
+digit=number%10;
+rev =rev*10+digit;
+number=number/10;
+
 }
-cout<<sum;
+cout<<rev;
 
-
+if(rev==org)
+{
+    cout<<"pallindrome";
+}
+else
+{
+ cout<<"not a pallindrome";
+} 
 }
 
-
-
-  
