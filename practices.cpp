073057ffc1078b03e76d7d68c9bf71fbff7d3d2f
surajkr502 <sawsurajkumar502@gmail.com>
2025-05-31 20:18:01 +0530

    swapping no.

--- practices.cpp
+++ practices.cpp
@@ -1,13 +1,66 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main()
 {
+    // # ASCII VALUE------------
+
+
     // char ch='c';
     // cout<<int(ch);
     // char ch=' ';
     // cout<<int(ch);
     // int n='a';
     // cout<<n;
+
+
+    // # WAP to cal sum and average----------
+
+    // int a,b,c;
+    // float average,sum;
+    // cout<<"Enter 3 Integers:";
+    // cin>>a>>b>>c;
+    // sum=a+b+c;
+    // average=sum/3;
+    // cout<<"The sum is:"<<sum<<endl;
+    // cout<<"The average is:"<<average;
+
+
+    // # Redusing variable in 3---------
+
+    // int a,b,c;
+    //  cout<<"Enter 3 Integers:";
+    // cin>>a>>b>>c;
+    // cout<<"The sum is:"<<a+b+c<<endl;
+    // cout<<"The average is:"<<(a+b+c)/3.0;
+
+
+    // # Area of circle and circumference---------
+
+    // float r ,area,circ;
+    // cout<<"Enter a radius of circle:";
+    // cin>>r;
+    // area=3.14*pow(r,2);
+    // circ=2*3.14*r;
+    // cout<<"Area of Circle is:"<<area<<endl;
+    // cout<<"Circumference of Circle is:"<<circ;
+
+
+    // # SWAPING THE NO.----------
+    int a,b,c;
+    cout<<"Enter 2 integers:";
+    cin>>a>>b;
+    cout<<"Before swapping 'a' is:"<<a<<endl<<"Before swapping 'b' is:"<<b<<endl;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"After swapping 'a' is:"<<a<<endl<<"After swapping 'b' is:"<<b;
+
+
+return 0;
+
+
+
     
 }
 
