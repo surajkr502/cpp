6a3af58923db092212dfc6395505326f0aed63f1
surajkr502 <sawsurajkumar502@gmail.com>
2025-09-27 22:56:32 +0530

    pointer

--- opps.cpp/opps.cpp
+++ opps.cpp/opps.cpp
@@ -30,35 +30,82 @@ using namespace std;
 // }
 
 
-class Emp
-{
-    private:
-        int age;
-        float salary;
-        char name[20];
-    public: 
-        void set(int,float,const char*);
-        void show();
-};
-void Emp::set(int a,float s,const char* p)
-{
-    age=a;
-    strcpy(name,p);
-    salary=s;
-}
-void Emp::show()
-{
-    cout<<age<<","<<salary<<","<<name<<endl;
-}
-int main()
-{
-    Emp E,F;
-    E.set(25,30000,"Rahul");
-    F.set(30,50000,"Mohan");
-    E.show();
-    F.show();
-    return 0;
-}
+
+// Creating parametrized member function in class 
+
+// class Emp
+// {
+//     private:
+//         int age;
+//         float salary;
+//         char name[20];
+//     public: 
+//         void set(int,float,const char*);
+//         void show();
+// };
+// void Emp::set(int a,float s,const char* p)
+// {
+//     age=a;
+//     strcpy(name,p);
+//     salary=s;
+// }
+// void Emp::show()
+// {
+//     cout<<age<<","<<salary<<","<<name<<endl;
+// }
+// int main()
+// {
+//     Emp E,F;
+//     E.set(25,30000,"Rahul");
+//     F.set(30,50000,"Mohan");
+//     E.show();
+//     F.show();
+//     return 0;
+// }
+
+// WAOOP to cal factorial of the number given by the user.
+// Make sure that your class should have seperated member functions for intialization,inputing,cal and displaying
+// class Factorial
+// {
+//     private:
+//     int a,f;
+//     public:
+//     void init();
+//     void get();
+//     void cal();
+//     void show();
+// };
+// void Factorial::init()
+// {
+//     f=1;
+// }
+// void Factorial::get()
+// {
+//     cout<<"Enter a no.:";
+//     cin>>a;
+// }
+// void Factorial::cal()
+// {
+//     for(int i=1;i<=a;i++)
+//     {
+//         f=f*i;
+//     }
+// }
+// void Factorial::show()
+// {
+//     cout<<"Factorial of no."<<a<<" is:"<<f;
+
+// }
+// int main()
+// {
+//     Factorial obj;
+//     obj.init();
+//     obj.get();
+//     obj.cal();
+//     obj.show();
+//     return 0;
+// }
+
 
 
 
---
+++ opps.cpp/pointer.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a=10;
+    // int*p;
+    // p=&a;
+    // cout<<"address of a is:"<<&a<<endl;
+    // cout<<"content of p is:"<<p<<endl;
+    // cout<<a<<endl;
+    // cout<<*p<<endl;
+    // a=20;
+    // cout<<a<<endl;
+    // cout<<*p<<endl;
+    // a=30;
+    // cout<<a<<endl;
+    // cout<<*p;
+
+    // int a = 10;
+    // int p;
+    // p = &a;
+    // cout << p;
+
+
+    // Accessing Array Using Pointer
+
+    // int arr[5];
+    // int *p;
+    // int i;
+    // p=arr;
+    // for (i=0;i<5;i++)
+    // {
+    //     cout<<"enter no.:"<<endl;
+    //     cin<<p;
+    // }
+    // for(i=10;i<5;i++)
+    // {
+    //     cout<<*(p+i);
+    // }
+
+
+
+
+    return 0;
+}
